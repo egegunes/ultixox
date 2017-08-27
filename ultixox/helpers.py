23bb993461c1c89118e18ea65ca1c408ff7ba159
Ege Güneş <egegunes@gmail.com>
2017-08-27 22:18:08 +0300

    Add helper function for checking board status

---
+++ ultixox/helpers.py
@@ -0,0 +1,19 @@
+def check_board(fields):
+    WINNERS = (
+        ((0, 0), (0, 1), (0, 2)),
+        ((1, 0), (1, 1), (1, 2)),
+        ((2, 0), (2, 1), (2, 2)),
+        ((0, 0), (1, 0), (2, 0)),
+        ((0, 1), (1, 1), (2, 1)),
+        ((0, 2), (1, 2), (2, 2)),
+        ((0, 0), (1, 1), (2, 2)),
+        ((0, 2), (1, 1), (2, 0)),
+    )
+
+    for WINNER in WINNERS:
+        field_1 = fields[WINNER[0][0]][WINNER[0][1]]
+        field_2 = fields[WINNER[1][0]][WINNER[1][1]]
+        field_3 = fields[WINNER[2][0]][WINNER[2][1]]
+
+        if field_1.value == field_2.value == field_3.value:
+            return field_1.value
