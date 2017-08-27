93d01d234890a77b9da4b41d1f2d04c7f050d10e
Ege Güneş <egegunes@gmail.com>
2017-08-27 22:31:54 +0300

    Add move and check methods to Game and Board

--- ultixox/board.py
+++ ultixox/board.py
@@ -1,3 +1,6 @@
+from helpers import check_board
+
+
 class Field:
     def __init__(self, row, column):
         self.row = row
@@ -12,8 +15,20 @@ class Board:
     def __init__(self, row, column):
         self.row = row
         self.column = column
+        self.value = None
         self.fields = {row: [Field(row, column) for column in range(3)] for row in range(3)}
 
     def __repr__(self):
         return f'Board({self.row}, {self.column})'
 
+    def move(self, value, row, column):
+        self.fields[row][column].value = value
+        self.check()
+
+    def check(self):
+        if self.value is not None:
+            return self.value
+
+        self.value = check_board(self.fields)
+
+        return self.value
--- ultixox/game.py
+++ ultixox/game.py
@@ -1,10 +1,18 @@
 from player import Player
 from board import Board
+from helpers import check_board
 
 
 class Game:
     def __init__(self):
         self.boards = {row: [Board(row, column) for column in range(3)] for row in range(3)}
+        self.status = None
 
-    def move(self, piece, board_row, board_column, field_row, field_column):
-        self.boards[board_row][board_column].fields[field_row][field_column].value = piece
+    def move(self, value, board_row, board_column, field_row, field_column):
+        self.boards[board_row][board_column].move(value, field_row, field_column)
+        if self.boards[board_row][board_column].value is not None:
+            self.check()
+
+    def check(self):
+        self.status = check_board(self.boards)
+
