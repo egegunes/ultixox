a3676df2205ee1cd9a8d48709681e0e2e5b2b4d1
Ege Güneş <egegunes@gmail.com>
2017-08-28 23:28:23 +0300

    Add players and turn attributes to Game

--- ultixox/game.py
+++ ultixox/game.py
@@ -7,6 +7,8 @@ class Game:
     def __init__(self):
         self.boards = [[Board(row, column) for column in range(3)] for row in range(3)]
         self.status = None
+        self.players = (Player(0), Player(1))
+        self.turn = self.players[0]
 
     def move(self, player, move):
         self.boards[move[0]][move[1]].move(player, move[2], move[3])
