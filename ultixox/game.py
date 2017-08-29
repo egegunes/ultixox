6a449ca8890a5306e93e2a18a55002e2b54f10d2
Ege Güneş <egegunes@gmail.com>
2017-08-30 00:41:00 +0300

    Force player to play in valid board

--- ultixox/game.py
+++ ultixox/game.py
@@ -5,15 +5,24 @@ from board import Board, FieldOccupied
 from helpers import check_board
 
 
+class InvalidBoard(ValueError):
+    pass
+
+
 class Game:
     def __init__(self):
         self.boards = [[Board(row, column) for column in range(3)] for row in range(3)]
         self.status = None
         self.players = (Player(0), Player(1))
         self.turn = self.players[0]
+        self.next_board = None
 
     def move(self, player, move):
+        if self.next_board and (move[0] != self.next_board.row or move[1] != self.next_board.column):
+            raise InvalidBoard(f'{self.next_board} is the only valid board to play.')
+
         self.boards[move[0]][move[1]].move(player, move[2], move[3])
+        self.next_board = self.boards[move[2]][move[3]]
 
     def check(self):
         self.status = check_board(self.boards)
@@ -28,7 +37,7 @@ if __name__ == '__main__':
 
             try:
                 game.move(game.turn.indicator, move)
-            except FieldOccupied:
+            except (FieldOccupied, InvalidBoard):
                 print("Invalid move. Try again.")
                 move = None
 
