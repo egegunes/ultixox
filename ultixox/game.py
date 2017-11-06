40b3dcdc50dd158c5b5def11fd3c5274e532b4c0
Ege Güneş <egegunes@gmail.com>
2017-11-06 23:52:45 +0300

    Get dummy move from computer

--- ultixox/game.py
+++ ultixox/game.py
@@ -6,11 +6,11 @@ from helpers import check_board
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, players):
+        self.players = players
+        self.turn = self.players[0]
         self.boards = [[Board(row, column) for column in range(3)] for row in range(3)]
         self.status = None
-        self.players = (Player(0), Player(1))
-        self.turn = self.players[0]
         self.next_board = None
 
     def move(self, player, move):
@@ -19,6 +19,7 @@ class Game:
 
         self.boards[move[0]][move[1]].move(player, move[2], move[3])
         self.next_board = self.boards[move[2]][move[3]]
+        print(f'Next board: {self.next_board}')
 
     def check(self, fields):
         self.status = check_board(fields)
@@ -28,11 +29,17 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game()
+    human = Player(0)
+    computer = Player(1, computer=True)
+    game = Game((human, computer))
     while game.status is None:
         move = None
         while move is None:
-            move = literal_eval(input(f'{game.turn} Move: '))
+            if game.turn is human:
+                move = literal_eval(input(f'{game.turn} Move: '))
+            else:
+                move = computer.get_move(game.state, game.next_board)
+                print(f'Computer: {move}')
 
             try:
                 game.move(game.turn.indicator, move)
--- ultixox/player.py
+++ ultixox/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, indicator):
+    def __init__(self, indicator, computer=False):
         self.indicator = indicator
+        self.computer = computer
 
     def __repr__(self):
         return f'Player {self.indicator}'
+
+    def get_move(self, state, next_board):
+        import random
+        return (next_board.row, next_board.column, random.randint(0, 2), random.randint(0, 2))
